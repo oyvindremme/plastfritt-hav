5d88866104833a975a569b734d53bc84adf889ec
Øyvind Remme <hey@oyvrem.com>
2020-04-05 21:32:47 +0200

    changed from StaticQuery to useStaticQuery hook. cleaner and much, much simpler

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,44 +1,42 @@
 import React from "react"
-import { graphql, StaticQuery, Link } from "gatsby"
+import { graphql, useStaticQuery, Link } from "gatsby"
 
 import BackgroundImage from "gatsby-background-image"
 
-const Header = () => (
-    <StaticQuery
-        query={graphql`
-            query {
-                headerBackgroundImage: file(relativePath: { eq: "header_img.jpg" }) {
+const Header = () => {
+
+    const data = useStaticQuery(graphql`
+        query {
+            headerBackgroundImage: file(relativePath: { eq: "header_img.jpg" }) {
                 childImageSharp {
                     fluid(quality: 80, maxWidth: 1920) {
                     ...GatsbyImageSharpFluid_withWebp
                     }
                 }
-                }
             }
-        `}
-        render={data => {
-            return (
-                <BackgroundImage
-                    tag="header"
-                    className="front-header"
-                    fluid={data.headerBackgroundImage.childImageSharp.fluid}
-                    backgroundColor="#333333"
-                >
-                    <h1 className="front-header__title">Bli med på laget</h1>
-                    <p className="front-header__sub-title">Ei fjøre uten plast</p>
-                    <p className="front-header__body-text">Miljøengasjert organisasjon med kjærlighet for havet</p>
+        }
+    `)
 
-                    <a className="front-header__cta-primary" href="#" target="_blank">
-                        Registrer din aksjon
-                    </a>
+    return (
+        <BackgroundImage
+            tag="header"
+            className="front-header"
+            fluid={data.headerBackgroundImage.childImageSharp.fluid}
+            backgroundColor="#333333"
+        >
+            <h1 className="front-header__title">Bli med på laget</h1>
+            <p className="front-header__sub-title">Ei fjøre uten plast</p>
+            <p className="front-header__body-text">Miljøengasjert organisasjon med kjærlighet for havet</p>
 
-                    <a href="#kontakt" className="front-header__cta-secondary">
-                        Kontakt oss
-                    </a>
-                </BackgroundImage>   
-            ) 
-        }}
-    />
-)
+            <a className="front-header__cta-primary" href="#" target="_blank">
+                Registrer din aksjon
+            </a>
+
+            <a href="#kontakt" className="front-header__cta-secondary">
+                Kontakt oss
+            </a>
+        </BackgroundImage>   
+    )
+}
 
 export default Header
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,10 +1,10 @@
 import React from "react"
-import { Link, graphql, StaticQuery } from "gatsby"
+import { Link, graphql, useStaticQuery } from "gatsby"
 import Img from "gatsby-image"
 
-const Nav = () => (
-    <StaticQuery
-        query={graphql`
+const Nav = () => {
+
+    const data = useStaticQuery(graphql`
         query {
             logo: file(relativePath: { eq: "logo/plastfritt_hav-hori_logo.png" }) {
                 childImageSharp {
@@ -14,22 +14,22 @@ const Nav = () => (
                 }
             }
         }
-        `}
-        render={data => (
-            <nav className="navigation">
-                <div className="navigation__logo">
-                    <Link to="/">
-                        <Img fixed={data.logo.childImageSharp.fixed} />
-                    </Link>
-                </div>
-                {/* <div className="navigation__log-in-link">
-                    <Link to="/login">
-                        Logg inn
-                    </Link>
-                </div> */}
-            </nav>
-        )}
-    />
-)
+    `)
+
+    return (
+        <nav className="navigation">
+            <div className="navigation__logo">
+                <Link to="/">
+                    <Img fixed={data.logo.childImageSharp.fixed} />
+                </Link>
+            </div>
+            {/* <div className="navigation__log-in-link">
+                <Link to="/login">
+                    Logg inn
+                </Link>
+            </div> */}
+        </nav>
+    )
+}
 
 export default Nav

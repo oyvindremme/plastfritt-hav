61f8217e9c3191011267acdc1e697427078b762f
Øyvind Remme <hey@oyvrem.com>
2020-04-12 17:01:56 +0200

    adjusted more media queries

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,8 +3,8 @@ import React from "react"
 const Footer = () => (
     <footer className="site-footer">
         <div className="container padding-top">
-            <p>&copy; Plastfritt Hav</p>
-            <p className="site-footer__designer-signature">Design: <a href="https://oyvrem.com" target="_blank">Øyvind Remme</a></p>
+            <p className="site-footer__footer-title">&copy; Plastfritt Hav</p>
+            <p>Alle rettigheter reservert</p>
         </div>
     </footer>
 )

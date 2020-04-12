e4bd9f73a58e087c8a3555483190b677b17dcf95
Øyvind Remme <hey@oyvrem.com>
2020-04-12 16:31:29 +0200

    added container to final-convert

--- src/components/FinalConvert.js
+++ src/components/FinalConvert.js
@@ -20,13 +20,15 @@ const FinalConvert = () => {
     return (
         <BackgroundImage
         tag="header"
-        className="final-convert container"
+        className="final-convert"
         fluid={data.headerBackgroundImage.childImageSharp.fluid}
         backgroundColor="#333333">
-            <h2 className="final-convert__title">Vi trenger din hjelp</h2>
-            <p>La oss ta i et tak og gjøre verden til et rennere og mer miljøvennlig sted</p>
-            <p>Hele organisasjonen er tuftet på frivillighet, «nærleiken og kjærleiken til havet», og vi setter stor pris på alle som vil bidra til å gjøre verden til et litt bedre sted for både mennesker, fugler og dyr.</p>
-            <p>Så nøl ikke med å ta kontakt, sammen kan vi gjøre en forskjell 💚</p>
+            <div className="container">
+                <h2 className="final-convert__title">Vi trenger din hjelp</h2>
+                <p>La oss ta i et tak og gjøre verden til et rennere og mer miljøvennlig sted</p>
+                <p>Hele organisasjonen er tuftet på frivillighet, «nærleiken og kjærleiken til havet», og vi setter stor pris på alle som vil bidra til å gjøre verden til et litt bedre sted for både mennesker, fugler og dyr.</p>
+                <p>Så nøl ikke med å ta kontakt, sammen kan vi gjøre en forskjell 💚</p>
+            </div>
         </BackgroundImage>
     )
 }

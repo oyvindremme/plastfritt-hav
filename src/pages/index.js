9bfe3b750c7bc182f508c5b6afaba75eb3104b88
oyvrem <hey@oyvrem.com>
2020-04-18 23:45:25 +0200

    changed primary seo domain

--- src/pages/index.js
+++ src/pages/index.js
@@ -25,16 +25,16 @@ const Hjem = () => {
                 <meta name="description" content="Miljøengasjert organisasjon med kjærlighet for havet" />
 
                 <meta property="og:type" content="website" />
-                <meta property="og:url" content="https://plastfritthav.netlify.com/" />
+                <meta property="og:url" content="https://plastfritthav.no/" />
                 <meta property="og:title" content="Plastfritt Hav | Bli med på laget" />
                 <meta property="og:description" content="Miljøengasjert organisasjon med kjærlighet for havet" />
-                <meta property="og:image" content={`https://plastfritthav.netlify.com${seoImage}`} />
+                <meta property="og:image" content={`https://plastfritthav.no${seoImage}`} />
 
                 <meta property="twitter:card" content="summary_large_image" />
-                <meta property="twitter:url" content="https://plastfritthav.netlify.com/" />
+                <meta property="twitter:url" content="https://plastfritthav.no/" />
                 <meta property="twitter:title" content="Plastfritt Hav | Bli med på laget" />
                 <meta property="twitter:description" content="Miljøengasjert organisasjon med kjærlighet for havet" />
-                <meta property="twitter:image" content={`https://plastfritthav.netlify.com${seoImage}`} />
+                <meta property="twitter:image" content={`https://plastfritthav.no${seoImage}`} />
             </Helmet>
             <Nav />
             <Header />

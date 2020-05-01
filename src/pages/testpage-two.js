76438a77c275c5de37dd63cf82761aeeb44c1ae4
oyvrem <hey@oyvrem.com>
2020-05-01 22:58:26 +0200

    removed testing pages for incremental build prodding

--- src/pages/testpage-two.js
+++
@@ -1,7 +0,0 @@
-import React from "react"
-
-const TestpageTwo = () => (
-    <h1>This is just a test page</h1>
-)
-
-export default TestpageTwo
--- src/pages/testpage.js
+++
@@ -1,7 +0,0 @@
-import React from "react"
-
-const Testpage = () => (
-    <h1>This is just a test page</h1>
-)
-
-export default Testpage
